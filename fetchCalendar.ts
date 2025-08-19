68682a418758458d185eaadfc58310530dee5fce
秉虎 <s96016641@gmail.com>
2025-08-19 23:07:15 +0800

    feat(calendar): add calendar fetching functionality and output to JSON

---
+++ fetchCalendar.ts
@@ -0,0 +1,46 @@
+import { fetchSinglePage } from "@/utils/fetcher";
+import { writeFile } from "jsonfile";
+import { validatePath } from "@/utils/dir";
+
+const outputPath = validatePath("./dist/calendar.json");
+
+interface calendarItem {
+  year: number;
+  semester: number;
+  title: string;
+  link: string;
+}
+
+const fetchCalendar = async () => {
+  const setUrl = "https://adeva.utaipei.edu.tw/p/412-1061-73.php";
+
+  const $ = await fetchSinglePage(setUrl);
+  const data = $("#Dyn_2_2 > div > div > section > div > div > div > p");
+
+  let results: calendarItem[] = [];
+
+  data.each((_, el) => {
+    $(el)
+      .find("a")
+      .each((_, element) => {
+        const text = $(element).text().trim(); // 本校114學年度下學期行事曆
+        const link = $(element).attr("href"); // /var/file/61/1061/img/673214137.pdf
+
+        if (!link) return; // Skip if no link is found
+
+        results.push({
+          year: parseInt(text.match(/(\d{3})/g)?.[0] || "0"),
+          semester: text.includes("上學期") ? 1 : 2,
+          title: text,
+          link: `https://adeva.utaipei.edu.tw${link}`,
+        });
+      });
+  });
+
+  await writeFile(outputPath, results, { spaces: 2, EOL: "\r\n" });
+};
+
+(async () => {
+  await fetchCalendar();
+  console.log("Calendar data fetched and saved successfully!");
+})();
--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -2,22 +2,13 @@ import { load, CheerioAPI } from "cheerio";
 
 const delay = (s: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, s));
 
-export const fetchSinglePage = async (url: string, options?: RequestInit): Promise<CheerioAPI> => {
-  await delay(100 + Math.random() * 500);
-  const html = await getResp(url, options);
-  if (html === null) {
-    throw new Error(`Could not find the page, [${options?.method || "GET"}] ${url}`);
-  }
-  return load(html);
-};
-
-async function getResp(url: string, options: RequestInit = {}): Promise<string | null> {
+async function retryFetcher(url: string, options: RequestInit = {}): Promise<Response> {
   let retry = 0;
   while (retry < 10) {
     try {
       const now = new Date();
       const response = await fetch(url, {
-        method: "GET",
+        method: options.method ? options.method : "GET",
         ...options,
       });
 
@@ -25,15 +16,36 @@ async function getResp(url: string, options: RequestInit = {}): Promise<string |
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
-      const html = await response.text();
       console.log(`[fetch] ${url} done. (${new Date().getTime() - now.getTime()}ms)`);
-      return html;
+      return response;
     } catch (e: any) {
       console.error(`[error] ${url} - Attempt ${retry + 1}: ${e.message}`);
       retry += 1;
       await delay(1000 * retry * retry);
     }
   }
-  console.error(`[error] ${url} - Failed after ${retry} attempts.`);
-  return null;
+  throw new Error(`Failed to fetch ${url} after ${retry} attempts.`);
 }
+
+export const fetchSinglePage = async (url: string, options?: RequestInit): Promise<CheerioAPI> => {
+  await delay(100 + Math.random() * 500);
+  const response = await retryFetcher(url, options);
+  const html = await response.text();
+  if (html === null) {
+    throw new Error(`Could not find the page, [${options?.method || "GET"}] ${url}`);
+  }
+  return load(html);
+};
+
+export const fetcher = {
+  get: (url: string, options?: RequestInit) => {
+    return retryFetcher(url, { method: "GET", ...options });
+  },
+  post: (url: string, options?: RequestInit) => {
+    return retryFetcher(url, { method: "POST", ...options });
+  },
+  download: async (url: string, options?: RequestInit) => {
+    const response = await retryFetcher(url, { method: "GET", ...options });
+    return response.blob();
+  },
+};
